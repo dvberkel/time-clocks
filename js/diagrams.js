2d9cd1c6a36f388c9a140d05a3bbd1e88720c56f
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-31 13:56:38 +0100

    abstract rendering of diagram

--- js/diagrams.js
+++ js/diagrams.js
@@ -15,29 +15,31 @@
     });
 
     var isRendered = { /* used to keep track of which slides are already rendered */ };
-    Reveal.addEventListener('diagram.introduction', function(event){
-	    if (!isRendered[event.type]) {
-	        isRendered[event.type] = true;
-	        var container = document.getElementById('diagram.introduction');
-	        container.addEventListener('mousedown', function(event){
-		        if (event.shiftKey) {
-		            system.createProcess();
-		        }
-	        });
+    function render(id, callback){
+        Reveal.addEventListener(id, function(event){
+	        if (!isRendered[event.type]) {
+	            isRendered[event.type] = true;
+                callback(id);
+	        }
+        });
+    }
+    render('diagram.introduction', function(){
+	    var container = document.getElementById('diagram.introduction');
+	    container.addEventListener('mousedown', function(event){
+		    if (event.shiftKey) {
+		        system.createProcess();
+		    }
+	    });
 
-	        new pwl.SystemView(system, container, { width: 640, height: 480 });
-	    }
+	    new pwl.SystemView(system, container, { width: 640, height: 480 });
     });
-    Reveal.addEventListener('diagram.happend-before', function(event){
-	    if (!isRendered[event.type]) {
-	        isRendered[event.type] = true;
-	        var container = document.getElementById('diagram.happend-before');
-            var happendBefore = new pwl.System();
-	        new pwl.SystemView(happendBefore, container, { width: 640, height: 480 });
-            var p = happendBefore.createProcess();
-            p.createEvent();
-            p.createEvent();
-	    }
+    render('diagram.happend-before', function(){
+	    var container = document.getElementById('diagram.happend-before');
+        var happendBefore = new pwl.System();
+	    new pwl.SystemView(happendBefore, container, { width: 640, height: 480 });
+        var p = happendBefore.createProcess();
+        p.createEvent();
+        p.createEvent();
     });
 
     window.system = system;
