67b46584be0da6c346bc830b13a205036720c98c
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-03 20:04:42 +0100

    Total ordering
    A total ordering is imposed by the logical clocks and a order of processes

--- js/diagrams.js
+++ js/diagrams.js
@@ -132,6 +132,29 @@
             q.createEvent();
             var q2 = q.createEvent();
             p1.sendMessageTo(q2);
+        },
+        'diagram.total-ordering': function(id){
+	        var container = document.getElementById(id);
+            var happendBefore = new pwl.System();
+	        new pwl.SystemView(happendBefore, container, { 'width': 640, 'height': 480 });
+            var p = happendBefore.createProcess(new pwl.Clock(2, 3));
+            var p1 = p.createEvent();
+            p.createEvent();
+            var q = happendBefore.createProcess(new pwl.Clock(1, 2));
+            q.createEvent();
+            var q2 = q.createEvent();
+            p1.sendMessageTo(q2);
+            var q = happendBefore.createProcess(new pwl.Clock(2, 1));
+            var q1 = q.createEvent();
+            var q2 = q.createEvent();
+            q.createEvent();
+            var r = happendBefore.createProcess(new pwl.Clock(1, 1));
+            var r1 = r.createEvent();
+            r.createEvent();
+            r.createEvent();
+            var r4 = r.createEvent();
+            r1.sendMessageTo(q1);
+            q2.sendMessageTo(r4);
         }
     };
     for (var id in diagrams) {
